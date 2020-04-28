44b957e83f411fdf06e21385324eaa15e65f42f7
RakuPhys <sute1drc14159@gmail.com>
2020-04-29 03:04:54 +0900

    first commit: physics measure prefab,U#

---
+++ PhysMeasure/PhysMeasure.cs
@@ -0,0 +1,44 @@
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Measurement : UdonSharpBehaviour
+{
+    public float smooth_vel = 0.2f;
+    public float smooth_acc = 0.2f;
+    private Vector3 acceleration = Vector3.zero;
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 position = Vector3.zero;
+    private Vector3 latestAcc = Vector3.zero;
+    private Vector3 latestVel = Vector3.zero;
+    private Vector3 latestPos = Vector3.zero;
+
+    private void Update()
+    {
+        position = transform.position;
+        velocity = calcVelocity(position, latestPos, latestVel);
+        acceleration = calcAcceleration(velocity, latestVel, latestAcc);
+        latestPos = position;
+        latestVel = velocity;
+        latestAcc = acceleration;
+    }
+    private Vector3 calcVelocity(Vector3 p, Vector3 lp, Vector3 lv)
+    {
+        return (1.0f - smooth_vel) * (p - lp) / Time.deltaTime + smooth_vel * lv;
+    }
+
+    private Vector3 calcAcceleration(Vector3 v, Vector3 lv, Vector3 la)
+    {
+        return (1.0f - smooth_acc) * (v - lv) / Time.deltaTime + smooth_acc * la;
+    }
+
+    public Vector3 getVelocity()
+    {
+        return velocity;
+    }
+    public Vector3 getAcceleration()
+    {
+        return acceleration;
+    }
+}
